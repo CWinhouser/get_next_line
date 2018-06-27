1237670c2246e793b10a23c00faeaee7ca9a17f6
Kyle TWOMEY <ktwomey@e4r10p5.wethinkcode.co.za>
2018-06-27 09:27:53 +0200

    JFGI

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ktwomey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/14 08:06:08 by ktwomey           #+#    #+#             */
-/*   Updated: 2018/06/27 08:08:27 by ktwomey          ###   ########.fr       */
+/*   Updated: 2018/06/27 09:27:30 by ktwomey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static int			ft_line(char **line, char **line_read)
+static int			ft_line(char **line, char **line_read, int fd)
 {
 	 char	*temp;
 	 int	n;
 
 	 n = 0;
-	 while((*line_read)[n] != '\n' && (*line_read)[n])
+	 while(line_read[fd][n] != '\n' && line_read[fd][n])
 		 n++;
-	 if((*line_read)[n] == '\n')
+	 if(line_read[fd][n] == '\n')
 	 {
-		 *line = ft_strsub(*line_read, 0 , n);
-		 temp = ft_strdup((*line_read) + n + 1);
-		 ft_strdel(&(*line_read));
+		 *line = ft_strsub(line_read[fd], 0 , n);
+		 temp = ft_strdup(line_read[fd] + n + 1);
+		 ft_strdel(&line_read[fd]);
 		 line_read = &temp;
-		 if((*line_read)[0] == '\0')
-			 ft_strdel(&(*line_read));
+		 if(line_read[fd][0] == '\0')
+			 ft_strdel(&line_read[fd]);
 	 }
-	 else if((*line_read) == '\0')
+	 else if(line_read[fd] == '\0')
 	 {
-		 *line = ft_strdup((*line_read));
-		 ft_strdel(&(*line_read));
+		 *line = ft_strdup(line_read[fd]);
+		 ft_strdel(&line_read[fd]);
 	 }
 	 return (1);
 }
@@ -46,25 +46,27 @@ int					get_next_line (const int fd, char **line)
 	char		buff[BUFF_SIZE + 1];
 //	char		*temp;
 
-	if(!(*line_read))
-		*line_read = ft_strnew(0);
-	while ((i = read(fd, buff, BUFF_SIZE) > 0))
+	i = 0;
+/*	if(!line_read[fd])
+		line_read[fd] = ft_strnew(0);
+*/	while (((i = read(fd, buff, BUFF_SIZE)) > 0))
 	{
-/*		if(!(*line_read))
-			*line_read = ft_strnew(0);
-*/		buff[i] = '\0';
-		*line_read = ft_strjoin(*line_read, buff);
-		//printf("%s", *line_read);
-		//printf("%d", i);
+		if(!line_read[fd])
+			line_read[fd] = ft_strnew(0);
+		buff[i] = '\0';
+		line_read[fd] = ft_strjoin(line_read[fd], buff);
+		printf("%s", line_read[fd]);
+		//printf("%d\n", i);
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
+	//printf("%d\n", i);
 	if (i < 0)
 		return (-1);
-	if ((i ==  0) && ((*line_read) == NULL || *line_read[0] == '\0'))
+	if ((i ==  0) && (line_read[fd] == NULL || line_read[fd][0] == '\0'))
 		return (0);
 	//printf ("%s", *line_read);
-	return(ft_line(line, line_read));
+	return(ft_line(line, line_read, fd));
 }
 
 int     main(int ac, char **av)
@@ -85,7 +87,7 @@ int     main(int ac, char **av)
    while (i--)
    {
        ret = get_next_line(fd,&line);
-//       printf("%s",line);
+       //printf("%s\n",line);
        //free(line);
    }
    return (0);
